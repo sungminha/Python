90d6a919a3ed7cf9d5e8c538842be121ba91df29
Sung Min Ha SurfacePro4SM <sungminha@hotmail.com>
2019-02-20 16:53:34 -0500

    using mpl compatible color values

--- image_analysis/image_analysis_plot_histogram_of_label_regions_from_intensity_image.py
+++ image_analysis/image_analysis_plot_histogram_of_label_regions_from_intensity_image.py
@@ -49,7 +49,7 @@ ax1 = fig.add_subplot(1,1,1)
 for label_value in np.arange(4):
   print("label_value: ",label_value)
   label_region = image_img[ label_img == label_value ]
-  ax1.hist(label_region, color = Set1_4.colors[label_value], label = ["Label Value ",label_value] )
+  ax1.hist(label_region, color = Set1_4.mpl_colors[label_value], label = ["Label Value ",label_value] )
 
 plt.show()
 ax1.savefig( figname = output, dpi=fig.dpi )
