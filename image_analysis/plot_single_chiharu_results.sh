7eb3d54a28df9bfc041a8199c496acd33c98a475
Sung Min Ha SurfacePro4SM <sungminha@hotmail.com>
2019-02-21 15:33:45 -0500

    plot for all 4 modalities

--- image_analysis/plot_single_chiharu_results.sh
+++ image_analysis/plot_single_chiharu_results.sh
@@ -4,26 +4,58 @@ cd "/cbica/home/sakoc/comp_space/Protocols/20190220_n3n4/output";
 
 dir="/cbica/home/sakoc/comp_space/Protocols/20190220_n3n4/output";
 
-t="ABLK_2016.05.14";
-s=`echo ${t} | cut -d_ -f1`;
-
-flair="${dir}/${t}_flair_LPS_r_SSFinal.nii.gz";
-flair_n3="${dir}/${t}_flair_LPS_r_SSFinal_n3.nii.gz";
-flair_n4="${dir}/${t}_flair_LPS_r_SSFinal_n4.nii.gz";
-manseg="${dir}/${t}_LPS_r_manualSegm.nii.gz";
-brainmask="/cbica/projects/brain_tumor/Brain_Tumor_2018/Protocols/3_Muse/ABLK/${t}/${t}_t1_LPS_N4_r_muse.nii.gz";
-
 script="/cbica/home/hasun/git_sungminha/Python/image_analysis/image_analysis_plot_histogram_of_label_regions_from_intensity_image.py";
-
-# module unload python;
-# module load python/anaconda/3;
-
-# source activate py3_7_conda_forge;
 
 outdir="/cbica/home/hasun/comp_space/190216_BiasCorrection_MaskTest/Histograms";
 mkdir -pv "${outdir}";
 
-python ${script} -i ${flair} -m ${brainmask} -l ${manseg} -o "${outdir}/${t}_flair.png" -t "${t}_Flair";
-python ${script} -i ${flair_n3} -m ${brainmask} -l ${manseg} -o "${outdir}/${t}_flair_n3.png" -t "${t}_Flair_N3";
-python ${script} -i ${flair_n4} -m ${brainmask} -l ${manseg} -o "${outdir}/${t}_flair_n4.png" -t "${t}_Flair_N4";
+t="ABLK_2016.05.14";
+s=`echo ${t} | cut -d_ -f1`;
+modality="flair";
+
+flair="${dir}/${t}_${modality}_LPS_r_SSFinal.nii.gz";
+flair_n3="${dir}/${t}_${modality}_LPS_r_SSFinal_n3.nii.gz";
+flair_n4="${dir}/${t}_${modality}_LPS_r_SSFinal_n4.nii.gz";
+manseg="${dir}/${t}_LPS_r_manualSegm.nii.gz";
+brainmask="/cbica/projects/brain_tumor/Brain_Tumor_2018/Protocols/3_Muse/${s}/${t}/${t}_t1_LPS_N4_r_muse.nii.gz";
+
+python ${script} -i ${flair} -m ${brainmask} -l ${manseg} -o "${outdir}/${t}_${modality}.png" -t "${t}_${modality}";
+python ${script} -i ${flair_n3} -m ${brainmask} -l ${manseg} -o "${outdir}/${t}_${modality}_n3.png" -t "${t}_${modality}_N3";
+python ${script} -i ${flair_n4} -m ${brainmask} -l ${manseg} -o "${outdir}/${t}_${modality}_n4.png" -t "${t}_${modality}_N4";
+
+modality="t1ce";
+
+flair="${dir}/${t}_${modality}_LPS_r_SSFinal.nii.gz";
+flair_n3="${dir}/${t}_${modality}_LPS_r_SSFinal_n3.nii.gz";
+flair_n4="${dir}/${t}_${modality}_LPS_r_SSFinal_n4.nii.gz";
+manseg="${dir}/${t}_LPS_r_manualSegm.nii.gz";
+brainmask="/cbica/projects/brain_tumor/Brain_Tumor_2018/Protocols/3_Muse/${s}/${t}/${t}_t1_LPS_N4_r_muse.nii.gz";
+
+python ${script} -i ${flair} -m ${brainmask} -l ${manseg} -o "${outdir}/${t}_${modality}.png" -t "${t}_${modality}";
+python ${script} -i ${flair_n3} -m ${brainmask} -l ${manseg} -o "${outdir}/${t}_${modality}_n3.png" -t "${t}_${modality}_N3";
+python ${script} -i ${flair_n4} -m ${brainmask} -l ${manseg} -o "${outdir}/${t}_${modality}_n4.png" -t "${t}_${modality}_N4";
+
+modality="t1";
+
+flair="${dir}/${t}_${modality}_LPS_r_SSFinal.nii.gz";
+flair_n3="${dir}/${t}_${modality}_LPS_r_SSFinal_n3.nii.gz";
+flair_n4="${dir}/${t}_${modality}_LPS_r_SSFinal_n4.nii.gz";
+manseg="${dir}/${t}_LPS_r_manualSegm.nii.gz";
+brainmask="/cbica/projects/brain_tumor/Brain_Tumor_2018/Protocols/3_Muse/${s}/${t}/${t}_t1_LPS_N4_r_muse.nii.gz";
+
+python ${script} -i ${flair} -m ${brainmask} -l ${manseg} -o "${outdir}/${t}_${modality}.png" -t "${t}_${modality}";
+python ${script} -i ${flair_n3} -m ${brainmask} -l ${manseg} -o "${outdir}/${t}_${modality}_n3.png" -t "${t}_${modality}_N3";
+python ${script} -i ${flair_n4} -m ${brainmask} -l ${manseg} -o "${outdir}/${t}_${modality}_n4.png" -t "${t}_${modality}_N4";
+
+modality="t2";
+
+flair="${dir}/${t}_${modality}_LPS_r_SSFinal.nii.gz";
+flair_n3="${dir}/${t}_${modality}_LPS_r_SSFinal_n3.nii.gz";
+flair_n4="${dir}/${t}_${modality}_LPS_r_SSFinal_n4.nii.gz";
+manseg="${dir}/${t}_LPS_r_manualSegm.nii.gz";
+brainmask="/cbica/projects/brain_tumor/Brain_Tumor_2018/Protocols/3_Muse/${s}/${t}/${t}_t1_LPS_N4_r_muse.nii.gz";
+
+python ${script} -i ${flair} -m ${brainmask} -l ${manseg} -o "${outdir}/${t}_${modality}.png" -t "${t}_${modality}";
+python ${script} -i ${flair_n3} -m ${brainmask} -l ${manseg} -o "${outdir}/${t}_${modality}_n3.png" -t "${t}_${modality}_N3";
+python ${script} -i ${flair_n4} -m ${brainmask} -l ${manseg} -o "${outdir}/${t}_${modality}_n4.png" -t "${t}_${modality}_N4";
 
